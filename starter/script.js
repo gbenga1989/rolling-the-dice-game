0cc030f5888cd83391781dced7182db159e3012b
gbenga <odetoyinbog@gmail.com>
2025-01-21 11:56:57 +0100

    21st_4

--- starter/script.js
+++ starter/script.js
@@ -20,12 +20,20 @@ diceEL.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0; 
+  currentScore = 0;
+  player0EL.classList.toggle('player--active');
+  player1EL.classList.toggle('player--active');
+}
 
 //rolling dice functionality
 btnRoll.addEventListener('click', function(){
+  if (playing) {
   //1.generate a random dce roll
 const dice = Math.trunc(Math.random() * 6) + 1;
-console.log(dice);
+
 
   //2.display dice
   diceEL.classList.remove('hidden');
@@ -38,10 +46,29 @@ currentScore += dice;
 document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else{
     // switch to the next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0; 
-    currentScore = 0;
-    player0EL.classList.toggle('player--active');
-    player1EL.classList.toggle('player--active');
+   switchPlayer();
   }
+}
+})
+
+btnHold.addEventListener('click', function(){
+  if(playing){
+
+  
+  //1. Add current score to the active player score
+  scores[activePlayer] += currentScore;
+document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+  //2. check if the players score is >=100
+  if (scores(activePlayer) >= 20){
+      //finish the game
+      playing = false;
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+
+  } else{
+    //3. switch to the next player
+  switchPlayer();
+  }
+}
 })
