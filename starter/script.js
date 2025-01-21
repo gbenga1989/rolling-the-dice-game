3872910cd243fa689159743d357b6431951759de
gbenga <odetoyinbog@gmail.com>
2025-01-21 10:24:21 +0100

    21st

--- starter/script.js
+++ starter/script.js
@@ -3,6 +3,8 @@
 // selecting elements
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
 const diceEL = document.querySelector('.dice')
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
@@ -13,12 +15,25 @@ score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add('hidden');
 
+let currentScore = 0;
 
 //rolling dice functionality
-btnRoll.addEventListener.addEventListener('click', function(){
+btnRoll.addEventListener('click', function(){
   //1.generate a random dce roll
 const dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
   //2.display dice
+  diceEL.classList.remove('hidden');
+  diceEL.src = `dice-${dice}.png`;
 
   //3.check for rolled 1:if true, switch player
+  if(dice !== 1){
+// Add the dice to the current score
+currentScore += dice;
+current0EL.textContent = currentScore; //CHANGE LATER
+
+  } else{
+    // switch to the next player
+  }
 })
