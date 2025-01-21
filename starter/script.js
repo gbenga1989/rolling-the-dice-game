d4cfd019ab6856caa8334f9e26c8abe8d2281ad2
gbenga <odetoyinbog@gmail.com>
2025-01-21 11:01:10 +0100

    20th_3

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting elements
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
 const current0EL = document.getElementById('current--0');
@@ -15,7 +17,9 @@ score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //rolling dice functionality
 btnRoll.addEventListener('click', function(){
@@ -31,9 +35,13 @@ console.log(dice);
   if(dice !== 1){
 // Add the dice to the current score
 currentScore += dice;
-current0EL.textContent = currentScore; //CHANGE LATER
-
+document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else{
     // switch to the next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; 
+    currentScore = 0;
+    player0EL.classList.toggle('player--active');
+    player1EL.classList.toggle('player--active');
   }
 })
